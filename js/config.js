c27682b73b8f03e212b0b8fe1616b1058ad0cdaa
james341 <taine049@gmail.com>
2021-10-12 12:17:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -134,6 +134,7 @@ var l_musicDisplay = true;
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
 	{ogg: "nightmarebchristmas.ogg", name: "Nations - Installation Music"},
+	{ogg: "pineradioseven.ogg", name: "Nations - Installation Music"},
 	{ogg: "monstermash.ogg", name: "Nations - Installation Music"},
 	{ogg: "spookyscarysk.ogg", name: "Nations - Installation Music"},
 	{ogg: "simpsonshitrunsev.ogg", name: "Nations - Installation Music"},
